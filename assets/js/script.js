7e827215495a2804a993bdb874ef65f74af92149
dev_vitorh <103538149+VitoorHugo1@users.noreply.github.com>
2023-11-21 19:18:08 -0300

    Atualizar o script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ mode.addEventListener('click', () => {
     if(mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
+
         form.classList.add('dark');
         return ;
     }
